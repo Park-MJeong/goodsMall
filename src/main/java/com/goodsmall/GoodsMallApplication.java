12d1abd97b52ae4be1e159b0884182cf55b2199d
Park-MJeong <pmj7511@gmail.com>
2024-12-18 14:26:31 +0900

    init: 초기환경셋팅

---
+++ src/main/java/com/goodsmall/GoodsMallApplication.java
@@ -0,0 +1,13 @@
+package com.goodsmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodsmall/GoodsMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodsmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
